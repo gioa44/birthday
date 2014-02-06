cbb52f2887733f1c666857651ffbd9f05b69257f
datoxxl <datoxxl@gmail.com>
2014-02-06 18:12:45 +0400

    Added new project Birthday.Tools with the class CodeGenerator

---
+++ Birthday.Tools/CodeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace Birthday.Tools
+{
+    public class CodeGenerator
+    {
+        private const string CharacterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+        public static string GetCode(int size)
+        {
+            var result = new char[9];
+
+            var characterArray = CharacterSet.ToCharArray();
+            var arr = new byte[1];
+
+            var rng = RNGCryptoServiceProvider.Create();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                do
+                {
+                    rng.GetNonZeroBytes(arr);
+                } while (arr[0] >= characterArray.Length);
+
+                result[i] = characterArray[arr[0]];
+            }
+
+            return new string(result);
+        }
+    }
+}
